f3eeb8943df874544bc6b44d389e00446f351103
Isaac J <jimenez.isaac.jr@gmail.com>
2024-05-04 00:19:33 -0700

    add init project, fetching response environment

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { useQuery, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+// Create a client
+const queryClient = new QueryClient();
+
+
+async function fetchDoggos() {
+  try {
+    const response = await fetch('https://dogapi.dog/api/v2/breeds');
+    return response.json();
+  } catch (err) {
+    console.error('Fetch error:', err);
+  }
+}
+
+function Doggos() {
+  const { isPending, isError, isSuccess, data, error } = useQuery({
+    queryKey: ['doggos'],
+    queryFn: fetchDoggos
+  });
+
+  if (isError) {
+    console.error(error);
+    return <Text>Errr</Text>;
+  }
+
+  if (isPending) {
+    return <Text>Fetching, woof...</Text>;
+  }
+
+  if (isSuccess) {
+    console.log("Great scott, it worked!!");
+  }
+
+  if (data) {
+    console.log(data);
+  }
+  return (
+    <ScrollView>
+      <View>
+        {data.data.map((obj) => {
+          return <Text key={obj.id}>{JSON.stringify(obj, null, 2)}</Text>;
+        })}
+      </View>
+    </ScrollView>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Doggos />
+      </View>
+    </QueryClientProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    minHeight: '100vh'
   },
 });
