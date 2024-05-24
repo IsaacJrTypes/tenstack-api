d32c1f3469571fd20394f2fba624e2af8ce93bbb
Isaac J <jimenez.isaac.jr@gmail.com>
2024-05-24 14:49:14 -0700

    add put mutation

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, Pressable, TextInput,SafeAreaView } from 'react-native';
 import {QueryClient,QueryClientProvider,useMutation,useQuery,useQueryClient} from '@tanstack/react-query';
+import { NativeWindStyleSheet } from "nativewind";
 
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 
 // create fetch all post api
@@ -28,24 +32,31 @@ async function createPost(newData) {
     console.log('response created', result)
     return result
   } catch (err) {
-    console.err(`Error from create post${err}`)
+    console.error(`Error from create post${err}`)
   }
 }
+
+
 
 // Create a client
 const queryClient = new QueryClient();
 
 
 function BlogPosts() {
-  const [post,setPost] = useState(null)
-  const [inputPost, setInputPost] = useState({ title: '', body: '', userId: '', id: '' }
-)
   const queryClient = useQueryClient()
 
-  const {data, isLoading, isError} = useQuery({
+  const [post,setPost] = useState(null)
+  const [editId, setEditId] = useState(null)
+  const [editPost, setEditPost] = useState({ title: '', body: '', userId: '', id: '' })
+  const [inputPost, setInputPost] = useState({ title: '', body: '', userId: '', id: '' })
+
+  const {data, isLoading, isError, refetch} = useQuery({
     queryKey: ['posts'],
-    queryFn: getAllPosts
+    queryFn: getAllPosts,
+    onSuccess: () => { console.log('triggered data query check')}
   });
+
+  
   // create a post
   const createMutation = useMutation({
     mutationKey: ['posts'],
@@ -54,20 +65,59 @@ function BlogPosts() {
       setPost(data)
       queryClient.invalidateQueries(['posts'])
     }
-  })
+  })  
+
+  const updateMutation = useMutation({
+    mutationKey: ['posts', editPost.id],
+    mutationFn: async (editPost) => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${editPost.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(editPost),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      const result = await response.json();
+      console.log('update fetch', result);
+      return result;
+    },
+    onSuccess: (fetchUpdate) => {
+      console.log('update triggered');
+      queryClient.setQueryData(['posts'], (oldData = []) => {
+        const result = oldData.map((post) => (post.id === fetchUpdate.id ? fetchUpdate : post));
+        console.log('updated post', result);
+        return result;
+      });
+
+      setEditId(null);
+    },
+  });
+
+  
+  useEffect(() => {
+    if(data) {
+      console.log('Data updated:',data[0])
+    }
+    ;
+  }, [data]);
 
 
-  const handleInputChange = (prop,value) => {
-    console.log("before:",inputPost)
+  const handleInputChange = (prop,value,callback) => {
+    //console.log("before:",inputPost)
     if (prop === 'userId') {
       value = parseInt(value)
     }
-    setInputPost((prev) => {
+    callback((prev) => {
       const updatedPost = { ...prev, [prop]: value };
       console.log('onchange:', updatedPost);
       return updatedPost;
     });
   }
+
+  const startEdit = (post) => {
+    setEditId(post.id);
+    setEditPost(post);
+  };
 
 
   if(isError) {
@@ -82,9 +132,9 @@ function BlogPosts() {
       <SafeAreaView>
       <ScrollView>
           <View className='flex flex-col justify-center items-center my-3'>
-            <TextInput className='border-gray-400 border-solid border-2 w-40' placeholder={"User ID"} onChangeText={(input)=>{handleInputChange('userId', input)}} />
-            <TextInput className='border-gray-400 border-solid border-2 w-40 my-3' placeholder={"Title"} onChangeText={(input)=>{handleInputChange('title', input)}} />
-            <TextInput className='border-gray-400 border-solid border-2 w-40 my-3' placeholder={"body"} onChangeText={(input)=>{handleInputChange('body', input)}} />
+            <TextInput className='border-gray-400 border-solid border-2 w-40' placeholder={"User ID"} onChangeText={(input) => { handleInputChange('userId', input, setInputPost)}} />
+            <TextInput className='border-gray-400 border-solid border-2 w-40 my-3' placeholder={"Title"} onChangeText={(input) => { handleInputChange('title', input, setInputPost)}} />
+            <TextInput className='border-gray-400 border-solid border-2 w-40 my-3' placeholder={"body"} onChangeText={(input) => { handleInputChange('body', input, setInputPost)}} />
             <Pressable className='border-solid border-2 border-sky-500'>
             <Text onPress={() => { createMutation.mutate({...inputPost})} } > Create Mutate Button</Text>
           </Pressable>
@@ -93,15 +143,25 @@ function BlogPosts() {
             return (
             <View className='border-solid border-green border-2 my-3'
                 key={post.id}>
-              <Text>UserID:{post.userId}</Text>
-              <Text>Title:{post.title}</Text>
-              <Text>body:{post.body}</Text>
-              <Pressable className='border-solid border-2 border-sky-500'>
-                <Text onPress={() => { console.log('Edit pressed')}} > Edit Button</Text>
-              </Pressable>
-              <Pressable className='border-solid border-2 border-sky-500'>
-                <Text onPress={() => { console.log('Save pressed') }} > Save Button</Text>
-              </Pressable>
+                {editId === post.id ? 
+                  (<View>
+                    <TextInput value= {editPost.userId.toString()} onChangeText={(input) => { handleInputChange('userid',input,setEditPost)}}/>
+                    <TextInput value={editPost.title}  onChangeText={(input) => { handleInputChange('title',input,setEditPost)}}/>
+                    <TextInput value={editPost.body}  onChangeText={(input) => { handleInputChange('body',input,setEditPost)}}/>
+                    <Pressable className='border-solid border-2 border-sky-500' onPress={() => updateMutation.mutate(editPost,data)}>
+                      <Text>Save</Text>
+                    </Pressable>
+                  </View>)
+                  :
+                  (<View>
+                      <Text>UserID: {post.userId}</Text>
+                      <Text>Title: {post.title}</Text>
+                      <Text>Body: {post.body}</Text>
+                    <Pressable className='border-solid border-2 border-sky-500' onPress={() => startEdit(post)}>
+                        <Text>Edit</Text>
+                      </Pressable>
+                    </View>)
+                }
             </View>)
           })}
         </View>
